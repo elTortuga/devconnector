cc68293eb12656cdccf28ec2b22a9633db982a9f
elTortuga <mds831@gmail.com>
2018-07-24 23:03:09 -0700

    Get rid of ugly console.log - clean up

--- validation/education.js
+++ validation/education.js
@@ -3,8 +3,6 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateExperienceInput(data) {
   let errors = {};
-
-  console.log(data);
   
   data.school = !isEmpty(data.school) ? data.school : '';
   data.degree = !isEmpty(data.degree) ? data.degree : '';
