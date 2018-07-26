da172beaf7d6c207d07fa9278d2faa43a7a5c1e2
elTortuga <mds831@gmail.com>
2018-07-25 22:35:25 -0700

    Basic Layot

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
+import Landing from './component/layout/Landing';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
