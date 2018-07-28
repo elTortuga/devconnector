c05082edc0c80adc6f2668fcbc4e517dbe27c332
elTortuga <mds831@gmail.com>
2018-07-28 13:12:38 -0700

    Registration and the error reducer

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import axios from "axios";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
-import { connect } from 'react-redux';
-import { registerUser } from '../../actions/authActions';
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
 
 class Register extends Component {
   constructor() {
@@ -20,6 +20,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors){
+      this.setState({errors: nextProps.errors});
+    } 
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -33,23 +39,17 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    
-    this.props.registerUser(newUser);
 
-    // axios
-    //   .post("/api/users/register", newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
     const { errors } = this.state;
 
-    const { user } = this.props.auth;
 
     return (
       <div className="register">
-        { user ? user.name : null }
+ 
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -134,11 +134,16 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
-const mapStateProps = (state) => ({
-  auth: state.auth
-})
+const mapStateProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
 
-export default connect(mapStateProps, { registerUser })(Register);
+export default connect(
+  mapStateProps,
+  { registerUser }
+)(withRouter(Register));
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,34 +1,10 @@
-import { TEST_DISPATCH } from '../actions/types';
-
 const initialState = {
   isAuthenticated: false,
   user: {}
 }
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    case TEST_DISPATCH:
-      return {
-        ...state,
-        user: action.payload
-      }
-  
-    // case value:
-      
-    //   break;
-  
-    // case value:
-      
-    //   break;
-  
-    // case value:
-      
-    //   break;
-  
-    // case value:
-      
-    //   break;
-  
+  switch (action.type) {  
     default:
       return state;
   }
