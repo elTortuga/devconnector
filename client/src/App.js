107ec1a19b16cb5623445bfcecbae331503bff1d
elTortuga <mds831@gmail.com>
2018-07-30 14:41:28 -0700

    Profile reducer and Dashboard Start

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './util/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
-
+import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from "react-redux";
 import store from './store'
@@ -13,6 +13,7 @@ import Footer from "./component/layout/Footer";
 import Landing from "./component/layout/Landing";
 import Register from "./component/auth/Register";
 import Login from "./component/auth/Login";
+import Dashboard from "./component/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -30,7 +31,8 @@ if(localStorage.jwtToken){
   if(decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current profile
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = './login';
   }
@@ -47,6 +49,8 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
+
             </div>
             <Footer />
           </div>
--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import classnames from "classnames";
 import { loginUser } from "../../actions/authActions";
 import TextFieldGroup from "../../common/TextFieldGroup";
 
@@ -78,7 +77,6 @@ class Login extends Component {
                   onChange={this.onChange}
                   error={errors.password}
                 />
-
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
