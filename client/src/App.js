7f3f3ddfaaf084c5277ef96dd2ca2bb0c9b0336f
elTortuga <mds831@gmail.com>
2018-07-30 16:45:02 -0700

    CreateProfile component and route

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,11 @@ class App extends Component {
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
